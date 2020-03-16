07518ce83ae2a2e3ff85c088bc783c8efaab133b
Fethi TChi <adm4algnw@gmail.com>
2020-03-16 18:03:16 +0100

    add few ForLoop forms

--- for-loop/main.go
+++ for-loop/main.go
@@ -1,12 +1,60 @@
+//The for loop is the only loop statement in Go.
+
+/*
+for initialization; condition; post {
+	// ... statments
+}
+*/
+
+/*
+// a traditional "while" loop
+for condition {
+	// ...
+}
+*/
+
+/*
+// a traditional infinite loop
+for {
+	// ...
+}
+*/
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var word string
 
-func main() {
-	word = "Hello Go!"
+//Standard ForLoop
+func Standard() {
+	word = "Hello from Standard!"
 	for i := 0; i < 5; i++ {
 		fmt.Println(word)
 	}
+}
+
+// ForLoop "for item in iterable object"
+func ForLoop() {
+	var FullName = []string{"DECHAICHA ", "Fethi "}
+	for _, name := range FullName {
+		fmt.Printf(name)
+	}
+	// make new line
+	fmt.Print("\n")
+}
+
+// ForLoopIndex "for item in iterable object"
+func ForLoopIndex() {
+	var FullName = []string{"DECHAICHA", "Fethi"}
+	for index, name := range FullName {
+		fmt.Println("index =", index, "and the name =", name)
+	}
+}
+
+func main() {
+	Standard()
+	ForLoop()
+	ForLoopIndex()
 }
